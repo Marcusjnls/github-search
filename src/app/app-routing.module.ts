8c300ac434872e8f83b54c3b20f409b4b165afae
Marcus Jean-Louis <marcusjnls@gmail.com>
2019-08-25 11:38:47 +0300

    Add routes to routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {RepositoriesComponent} from './repositories/repositories.component';
+import {GithomeComponent} from './githome/githome.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'profiles', component: GithomeComponent},
+  { path: 'repos', component: RepositoriesComponent},
+  { path: '', redirectTo:"/profiles", pathMatch:"full"},
+  { path:'**', component:GithomeComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
